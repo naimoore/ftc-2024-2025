512bea686e8e8c3a3e1a3f6f98d66f147a77dc22
chudgins-ftc <chudgins@philasd.org>
2024-11-13 15:26:34 -0500

    Update Rack_Test.java
    Removed tests.

--- Rack_Test.java
+++ Rack_Test.java
@@ -7,9 +7,6 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-//test
-
-//fork test
 
 @TeleOp(name="Rack Test", group="2024-2025")
 public class Rack_Test extends LinearOpMode {
